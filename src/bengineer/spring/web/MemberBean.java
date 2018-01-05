00c2464fb1231701c9ad19d6dfe2a1c7c8fba412
Administrator <Administrator@WIN7-20170808KX>
2018-01-05 20:05:29 +0900

    jj

--- src/bengineer/spring/web/MemberBean.java
+++ src/bengineer/spring/web/MemberBean.java
@@ -3,6 +3,7 @@ package bengineer.spring.web;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller()
@@ -11,14 +12,20 @@ public class MemberBean {
 	@Autowired
 	private SqlSessionTemplate sqlSession = null;
 	@RequestMapping("beLogin.do")
-	public String beLogin() {return "/BEngineer/beMember/beLogin";}
+	public String beLogin() {return "beMember/beLogin";}
 	
 	@RequestMapping("beCollback.do")
-	public String beCollback() {return "/BEngineer/beMember/beLogin";}
+	public String beCollback() {return "beMember/beLogin";}
 	
 	@RequestMapping("beAddinfo.do")
-	public String beAddinfo() {return "/BEngineer/beMember/beAddinfo";}
-	
+	public String beAddinfo() {return "beMember/beAddinfo";}
+
 	@RequestMapping("beChecknickname.do")
-	public String beChecknickname() {return "/BEngineer/beMember/beChecknickname";}
+	public String beChecknickname(Model model, String nickname ) {
+		Integer check = (Integer)sqlSession.selectOne("bengineer.beChecknickname",nickname);
+		System.out.println(check);
+		model.addAttribute("check",check);
+		model.addAttribute("nickname",nickname);
+		return "beMember/beChecknickname";
+	}
 }
